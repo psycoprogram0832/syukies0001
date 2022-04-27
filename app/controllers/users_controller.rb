67745432d7e44bf1ab5843dd603456279ce25224
psycoprogram0832 <raidoubiz0832@gmail.com>
2022-04-28 00:21:32 +0900

    詳細ページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def show
-    @profile = current_user.profile
-    @syuki_name = current_user.syuki_name
-    @since = current_user.since
-    @nickname = current_user.nickname
+    user = User.find(params[:id])
+    @syukies = Syukie.where("user_id = #{params[:id]}")
+    # @response = syukeis::where(“user_id”,  “1”)->get();
+    # @syukies = syukie.where(user_id = params[:id])
+    @profile = user.profile
+    @syuki_name = user.syuki_name
+    @since = user.since
+    @nickname = user.nickname
   end
 end
