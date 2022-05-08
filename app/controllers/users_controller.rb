0018bcc694a8b7536beee719884cc29b8bae9994
psycoprogram0832 <raidoubiz0832@gmail.com>
2022-05-09 07:29:08 +0900

    formエラー解消

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,26 @@ class UsersController < ApplicationController
 
   def grade
     user = User.find(params[:id])
-    @syukies = Syukie.where("user_id = #{params[:id]}")
+    # @syukies = Syukie.where("user_id = #{params[:id]}")
+    @syukie = Syukie.where("user_id = #{params[:id]}").first
     @profile = user.profile
     @syuki_name = user.syuki_name
     @since = user.since
     @nickname = user.nickname
+    # @param_count = (param_count)
+  end
+
+  def param_count
+    user = User.find(params[:id])
+    @syukies = Syukie.where("user_id = #{params[:id]}")
+    numbers = [] 
+    @points = user.syukies.points
+    numbers <= @points
+    numbers.length
+    sum_of_number = 0
+    numbers.each do |number|
+    sum_of_number = sum_of_number + number # 順番に足す
+    end
+    puts sum_of_number / numbers.length
   end
 end
