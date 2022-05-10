d46d66f3e4b9bf37e42a9bc24ce425f636cb6ed3
psycoprogram0832 <raidoubiz0832@gmail.com>
2022-05-11 01:29:41 +0900

    ver.1.2(showエラー)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,7 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
     @syukies = Syukie.where("user_id = #{params[:id]}")
-    # @response = syukeis::where(“user_id”,  “1”)->get();
-    # @syukies = syukie.where(user_id = params[:id])
-    @profile = user.profile
-    @syuki_name = user.syuki_name
-    @since = user.since
-    @nickname = user.nickname
   end
 
   def grade
