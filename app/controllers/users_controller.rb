a53e1e5e84a61e02f6d647e40fff8cb352afa726
psycoprogram0832 <raidoubiz0832@gmail.com>
2022-05-16 04:54:51 +0900

    S3導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,12 +31,7 @@ class UsersController < ApplicationController
         dates << syukie.date
       end
     end
-    # diarys = {}
-    # syukie_array.each do |syukie|
-    #   if syukie.created_at <= now && syukie.created_at >= now.ago(7.days)
-    #     diarys << syukie.dates.max.diary
-    #   end
-    # end
+    
     @ave = points.sum(0.0) / points.length
     @highdate = dates.max
     @lowdate = dates.min
